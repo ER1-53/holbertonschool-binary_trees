be2e900ebb5fcbef552d308118b621677a5719e7
PereDeMacron <peredemacron@gmail.com>
2023-09-12 15:22:23 +0200

    error betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_is_root -  verifie si la node donner est le root
- * 
- * @node: la node specifiée
- */
+* binary_tree_is_root -  verifie si la node donner est le root
+*
+* @node: la node specifiée
+*/
 
 
 int binary_tree_is_root(const binary_tree_t *node)
