8193778d0338fa69eea2bf8435926c7a91279907
PereDeMacron <peredemacron@gmail.com>
2023-09-12 14:27:00 +0200

    fix task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,17 +2,25 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t left_height = 0;
+	size_t right_height = 0;
+	size_t height = 1;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left != NULL)	
-		left_height = binary_tree_height(tree->left) + 1;
-	if (tree->right != NULL)
-		left_height = binary_tree_height(tree->right) + 1;
 
-	if (left_height > right_height)
-		return (left_height);
-	if (left_height < right_height)	
-		return (right_height);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		if (left_height > right_height)
+			height = left_height + 1;
+		else
+			height = right_height + 1;
+	}
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	return (height);
 }
