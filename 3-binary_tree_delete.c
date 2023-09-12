20eaa46519fb56b2744027a01e8c3d89fa60a111
PereDeMacron <peredemacron@gmail.com>
2023-09-12 15:15:44 +0200

    error betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,18 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary-tree-delete - Supprime l'arbre avec toutes les nodes
- * 
- * @tree: l'arbre binaire
- */
+* binary-tree-delete - Supprime l'arbre avec toutes les nodes
+*  
+* @tree: l'arbre binaire
+*/
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return;
+	if (tree == NULL)
+		return;
 
-    binary_tree_delete(tree->left);
-    binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
 
-    free(tree);
+	free(tree);
 }
