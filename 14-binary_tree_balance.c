a515d14458e68a231179a4d65e5cc37a88b3ca92
ER1-53 <cremey.erwan@gmail.com>
2023-09-13 12:25:58 +0200

    update 14 left initialisation left_height and right_height

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,17 +1,15 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_height - Calcule la hauteur d'un arbre binaire.
+ * binary_tree_height - measures the height of a binary tree
  *
- * @tree: Pointeur vers le nœud racine de l'arbre.
+ * @tree: pointer on parent's adress
  *
- * Return: La hauteur de l'arbre.
+ * Return: measures the height
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0;
-	size_t right_height = 0;
+	size_t left_height;
+	size_t right_height;
 	size_t height = 1;
 
 	if (tree == NULL)
@@ -20,41 +18,35 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height >= right_height)
-	{
-		height = left_height + 1;
-	}
-	else
-	{
-		height = right_height + 1;
-	}
+		if (left_height >= right_height)
+		{
+			height = left_height + 1;
+		}
+		else
+		{
+			height = right_height + 1;
+		}
 
 	return (height);
 }
 
 /**
- * binary_tree_balance - Counts the number of leaf nodes in a binary tree.
+ * binary_tree_balance - measures the balance factor of a binary tree
  *
- * @tree: Pointer to the root node of the tree.
+ * @tree: pointer on parent's adress
  *
- * Return:
- * - Number of leaf nodes.
- * - If tree is NULL, return 0.
+ * Return: measures the balance
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-
-	int balance_right = 0;
-	int balance_left = 0;
+	int left_height;
+	int right_height;
 
 	if (tree == NULL)
-	{
 		return (0);
-	}
 
-	balance_left = binary_tree_height(tree->left);
-	balance_right = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-	return (balance_left - balance_right);
+	return (left_height - right_height);
 }
