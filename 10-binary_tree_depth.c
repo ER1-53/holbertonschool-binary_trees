c3b66e94356f28620469ce447c0e9a0d17d1de9c
ER1-53 <cremey.erwan@gmail.com>
2023-09-13 12:12:28 +0200

    update function 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,12 +9,11 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	int count = 0;
-	if (!tree)
+	if (tree == NULL)
+		return (0);
+	
+	if (tree->parent == NULL)
 		return (0);
 
-	if (binary_tree_depth(tree->left) || binary_tree_depth(tree->right))
-
-	return (count);
-
+	return (1 + binary_tree_depth(tree->parent));
 }
