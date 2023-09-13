3790aac0c39d4769d78e5798e90af69ca7e3acd6
ER1-53 <cremey.erwan@gmail.com>
2023-09-13 15:16:43 +0200

    creat prototype for funct

--- binary_trees.h
+++ binary_trees.h
@@ -37,12 +37,17 @@ typedef struct binary_tree_s avl_t;
 
 /*-Erwan-*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
+size_t binary_tree_leaves(const binary_tree_t *tree);
 
 /*-Gary-*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
