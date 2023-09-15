c021f89032c35627bd15b4232c9f4f49fb69ae9d
PereDeMacron <peredemacron@gmail.com>
2023-09-15 15:15:58 +0200

    betty doc style for task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,5 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_trees_ancestor - trouve l'ancetre de deux noeuds
+ *
+ * @first: premier noeud
+ * @second: second noeud
+ *
+ * Return: retourne l'ancetre en commun des deux noeuds ou retourne NULL si
+ * aucun ancetre na ete trouver
+*/
+
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 	binary_tree_t *tmp;
