9bc46a49a3a2b74002d49cd8e02a62394074dfca
PereDeMacron <peredemacron@gmail.com>
2023-09-15 15:07:55 +0200

    advanced task 19

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -2,9 +2,22 @@
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
+	binary_tree_t *tmp;
 
+	if (first == NULL || second == NULL)
+		return (NULL);
 
-
-
-	
+	tmp = (binary_tree_t *)second;
+	while (first)
+	{
+		while (second)
+		{
+			if (first == second)
+				return ((binary_tree_t *)first);
+			second = second->parent;
+		}
+		second = tmp;
+		first = first->parent;
+	}
+	return (NULL);
 }
